54c34ee0e3ff6268b3e90ce5841f8cd52e67ebf0
Anirudha619 <anirudhanew999@gmail.com>
2024-07-27 20:28:04 +0530

    update lobster fee logic

--- app/controllers/fee.py
+++ app/controllers/fee.py
@@ -14,45 +14,74 @@ from utils.pd import BrokerFee
 from decimal import Decimal, getcontext
 from web3 import Web3
 import os
+import requests
 
-def is_lobster(user_address):
+alchemy_api_key = os.getenv("ALCHEMY_KEY")
+
+def get_nft_owners_for_collection():
+    contract_address = '0x026224A2940bFE258D0dbE947919B62fE321F042'
+    base_url = "https://eth-mainnet.alchemyapi.io/v2/"  # Replace with your Alchemy URL
+    url = f"{base_url}{alchemy_api_key}/getOwnersForCollection"
+
+    params = {
+        'contractAddress': contract_address
+    }
+    
     try:
-        # Set up the Infura provider
-        infura_url = f'https://mainnet.infura.io/v3/{os.getenv("INFURA_KEY")}'
-        web3 = Web3(Web3.HTTPProvider(infura_url))
-        user_address = web3.to_checksum_address(user_address)
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an exception for HTTP errors
 
-        # Contract address and ABI
-        contract_address = '0x026224A2940bFE258D0dbE947919B62fE321F042'
-        abi = [
-            {
-                "constant": True,
-                "inputs": [
-                    {"name": "owner", "type": "address"}
-                ],
-                "name": "balanceOf",
-                "outputs": [
-                    {"name": "balance", "type": "uint256"}
-                ],
-                "payable": False,
-                "stateMutability": "view",
-                "type": "function"
-            }
-        ]
+        data = response.json()
+        print(data)
+        print(len(data['ownerAddresses']))
+        # # Check if response contains valid data
+        if 'ownerAddresses' in data:
+            return data['ownerAddresses']
+        else:
+            raise Exception("Invalid response structure from Alchemy API")
 
-        # Create a contract instance
-        contract = web3.eth.contract(address=contract_address, abi=abi)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {str(e)}")
+        return []
 
-        # Call the balanceOf function
-        balance = contract.functions.balanceOf(user_address).call()
-        print(f"Balance of {user_address}: {balance}")
-        # print(balance)
+def get_perp_addresses_with_lobsternft():
+    broker_id='sharpe_ai'
+    base_url = "https://api-evm.orderly.org/v1/get_account"
 
-        return balance > 0
+    addresses = get_nft_owners_for_collection()
+    
+    results = []
+    for address in addresses:
+        try:
+            url = f"{base_url}?broker_id={broker_id}&address={address}"
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an exception for HTTP errors
 
-    except Exception as e:
-        print('Error retrieving balance:', e)
-        return False
+            data = response.json()
+            print(f"Orderly API response for address {address}:", data)
+            if(data['success'] == True):
+              results.append({
+                  'address': address,
+                  'accountId': data['data']["account_id"]
+              })
+
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred while checking address {address}: {str(e)}")
+
+    return results
+
+def is_address_in_array(address_to_check, array_of_results):
+    # Convert the address to lowercase for case-insensitive comparison
+    address_to_check_lower = address_to_check.lower()
+
+    # Iterate through the array of results
+    for result in array_of_results:
+        # Convert each address in the array to lowercase and compare
+        if result['address'].lower() == address_to_check_lower:
+            return True
+
+    # Address not found in the array
+    return False
 
 logger = setup_logging()
 config = ConfigLoader.load_config()
@@ -122,6 +151,10 @@ def update_user_rate_base_volume():
     user_fee = BrokerFee(_type="broker_user_fee")
     special_rate_whitelists = config["rate"]["special_rate_whitelists"]
     data = []
+    lobster_users = get_perp_addresses_with_lobsternft()
+    is_lobster_fee_updated = {}
+    lobster_maker_fee = 0.00024
+    lobster_taker_fee = 0.00054
     while True:
         _data = get_broker_users_volumes(_count)
         if not _data or not _data.get("data") or not _data["data"].get("rows"):
@@ -140,9 +173,7 @@ def update_user_rate_base_volume():
                     )
                     old_user_fee = user_fee.pd.query_data(_account_id)
                     # Lobster NFT logic
-                    is_nft_holder = is_lobster(_address)
-                    lobster_maker_fee = 0.00024
-                    lobster_taker_fee = 0.00054
+                    is_nft_holder = is_address_in_array(_address, lobster_users)
                     if not old_user_fee.empty:
                         _old_futures_maker_fee_rate = Decimal(
                             old_user_fee.futures_maker_fee_rate.values[0]
@@ -165,6 +196,9 @@ def update_user_rate_base_volume():
                                 _ret = {}
 
                                 if is_nft_holder and maker_fee_rate > lobster_maker_fee and taker_fee_rate > lobster_taker_fee:
+                                    is_lobster_fee_updated[_address.lower()] = {
+                                        "updated": True 
+                                    }
                                     _ret = {
                                         "account_id": _account_id,
                                         "futures_maker_fee_rate": lobster_maker_fee,
@@ -191,6 +225,9 @@ def update_user_rate_base_volume():
                         _ret = {}
 
                         if is_nft_holder and maker_fee_rate > lobster_maker_fee and taker_fee_rate > lobster_taker_fee:
+                            is_lobster_fee_updated[_address.lower()] = {
+                                "updated": True 
+                            }
                             _ret = {
                                 "account_id": _account_id,
                                 "futures_maker_fee_rate": lobster_maker_fee,
@@ -208,6 +245,17 @@ def update_user_rate_base_volume():
                         user_fee.create_update_user_fee_data(_ret)
         _count += 1
         time.sleep(2)
+    
+    for item in lobster_users:
+        if (item["address"].lower() in is_lobster_fee_updated) == False:
+            _ret = {
+                "account_id": item["accountId"],
+                "futures_maker_fee_rate": lobster_maker_fee,
+                "futures_taker_fee_rate": lobster_taker_fee,
+                "address": item["address"],
+            }
+            data.append(_ret)
+            
 
     # 5.request the batch fee interface: data
     set_broker_user_fee(data)
